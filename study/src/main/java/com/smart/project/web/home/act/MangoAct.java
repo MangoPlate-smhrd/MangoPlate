ad8de84b0b2a9d6af80b5a1b45cb4ca66b5988e4
smhrd <oocheol@naver.com>
2022-06-27 15:32:19 +0900

    mango template

---
+++ study/src/main/java/com/smart/project/web/home/act/MangoAct.java
@@ -0,0 +1,35 @@
+package com.smart.project.web.home.act;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class MangoAct {
+
+    @RequestMapping("/index")
+    public void index() {
+
+    }
+
+    @RequestMapping("/generic")
+    public void test() {
+
+    }
+    @RequestMapping("/main")
+    public void main() {
+
+    }
+
+    @RequestMapping("/elements")
+    public void elements() {
+
+    }
+
+
+
+
+}
